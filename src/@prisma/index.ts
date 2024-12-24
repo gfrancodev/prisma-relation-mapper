7b34b1731cef18fd2300104d380227c97f71d2ef
Gustavo Franco <guatavojorgee2511@gmail.com>
2024-12-24 14:36:24 -0300

    feat: 🎉 Add index files for Prisma utilities, exporting mapRelations and related functions

---
+++ src/@prisma/index.ts
@@ -0,0 +1,2 @@
+export { default as mapRelations } from './utils/map-relations';
+export { cleanAndSortRelations, processNestedRelations } from './utils/map-relations';
---
+++ src/@prisma/utils/index.ts
@@ -0,0 +1,2 @@
+export { default as mapRelations } from './map-relations';
+export { cleanAndSortRelations, processNestedRelations } from './map-relations'; 
